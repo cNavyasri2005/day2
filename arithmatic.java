8705a32dd2bfdac93f1083358059f9df52f1e2a6
cNavyasri2005 <cnavyareddynavya@gmail.com>
2026-02-07 15:02:14 +0530

    Add files via upload

---
+++ arithmatic.java
@@ -0,0 +1,15 @@
+public class arithmatic
+{
+    public static void main(String[]args)
+    {
+    int a=7;
+    int b=14;
+    int sum=a+b;
+    System.out.println(sum);
+    System.out.println(a-b);
+    System.out.println(a*b);
+    System.out.println(a%b);
+    System.out.println(a/b);
+
+}
+}
