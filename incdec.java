b3f90af2d09f150f1c53bab8fecbe20c13a2cbc0
cNavyasri2005 <cnavyareddynavya@gmail.com>
2026-02-07 14:49:01 +0530

    Add files via upload

---
+++ incdec.java
@@ -0,0 +1,12 @@
+public class incdec
+{
+    public static void main(String[]args)
+    {
+    int a=7;
+    System.out.println(a++);
+    System.out.println(a--);
+    System.out.println(--a);
+    System.out.println(++a-);
+
+}
+}
