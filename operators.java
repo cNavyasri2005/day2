ae2a7649202637bb32f8a9eb99d95159e5eee1f3
cNavyasri2005 <cnavyareddynavya@gmail.com>
2026-02-07 14:45:13 +0530

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,28 @@
+public class operators{
+    public static void main(String[]args){
+    int a=10;
+    int b=20;
+    int c=10;
+    System.out.println(a+b);
+    if(a>0)
+        {
+            System.out.println("positive num");
+        }
+        else
+        {
+            System.out.println("negative number");
+        }
+        if(a>0 && a%2==0)
+        {
+            System.out.println("is positive and even");
+        
+        }
+        else{
+            System.out.println("is negative and not even number");
+        }
+        System.out.println(c);
+        System.out.println(+b);
+
+    
+}
+}
