9ec6e6ee2c1240e693a6f58fc2f9b686dfe1de8c
cNavyasri2005 <cnavyareddynavya@gmail.com>
2026-02-07 14:46:49 +0530

    Add files via upload

---
+++ logical.java
@@ -0,0 +1,14 @@
+class logical
+{
+    public static void main(String[]args)
+    {
+    int a=90;
+    if(a>=85 && a<=100)
+    {
+        System.out.println("First");
+    }
+    else if(a>55 || a>100){
+        System.out.println("second");
+    }
+}
+}
