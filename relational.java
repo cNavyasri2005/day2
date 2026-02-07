c45dffbc2a2516b1e3c755b6f4a459ba74e03097
cNavyasri2005 <cnavyareddynavya@gmail.com>
2026-02-07 14:47:52 +0530

    Add files via upload

---
+++ relational.java
@@ -0,0 +1,13 @@
+public class relational
+{
+    public static void main(String []ar)
+    {
+        int a=5;
+        int b=10;
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a==b);
+        System.out.println(a!=b);
+
+    }
+}
