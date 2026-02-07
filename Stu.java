6d3e1ca998c5e2a387f1c9438132398c29c77b25
cNavyasri2005 <cnavyareddynavya@gmail.com>
2026-02-07 10:59:38 +0530

    Add files via upload

---
+++ Stu.java
@@ -0,0 +1,13 @@
+public class Stu
+{
+    public static void main(String[]arg)
+    {
+        int sub1=90;
+        int sub2=85;
+        int sub3=95;
+        int res=sub1+sub2+sub3;
+        int avg=sub1+sub2+sub3/3;
+        System.out.println(res);
+        System.out.println(avg);
+    }
+}
